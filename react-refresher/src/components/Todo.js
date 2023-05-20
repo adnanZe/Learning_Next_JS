bd51142943a3d19960618e98c58dc183d2fed7bd
AdnanZe <adnan.zecheria@yahoo.com>
2023-05-20 15:33:09 +0300

    skip the refresher

--- react-refresher/src/components/Todo.js
+++ react-refresher/src/components/Todo.js
@@ -1,9 +1,15 @@
 function Todo({ text }) {
+  function handleDelete() {
+    console.log("Clicked!");
+  }
+
   return (
     <div className="card">
       <h2>{text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={handleDelete}>
+          Delete
+        </button>
       </div>
     </div>
   );

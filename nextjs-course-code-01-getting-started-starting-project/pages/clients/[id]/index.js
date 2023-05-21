e7943515e2ba16985df7346b6cebb7c0697f64a7
AdnanZe <adnan.zecheria@yahoo.com>
2023-05-21 18:15:25 +0300

    navigating programmatically

--- nextjs-course-code-01-getting-started-starting-project/pages/clients/[id]/index.js
+++ nextjs-course-code-01-getting-started-starting-project/pages/clients/[id]/index.js
@@ -5,9 +5,20 @@ export default function ClientProjectPage() {
 
   console.log(router.query);
 
+  function loadProjectHandler() {
+    // load data...
+    // router.push("/clients/max/project");
+    // router.replace("/clients/max/project");
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  }
+
   return (
     <div>
       <h1>The Project of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }

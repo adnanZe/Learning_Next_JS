7a3de49c722789bf71b4cab0a552948bf8aca80b
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-05-23 19:56:43 +0300

    events search

--- nextjs-course-code-03-prj-routing-starting-setup/components/ui/buttons.js
+++ nextjs-course-code-03-prj-routing-starting-setup/components/ui/buttons.js
@@ -1,10 +1,18 @@
 import Link from "next/link";
 import classes from "./button.module.css";
 
-export default function Button({ link, children }) {
+export default function Button({ link, children, onClick }) {
+  if (link) {
+    return (
+      <Link href={link} className={classes.btn}>
+        {children}
+      </Link>
+    );
+  }
+
   return (
-    <Link href={link} className={classes.btn}>
+    <button className={classes.btn} onClick={onClick}>
       {children}
-    </Link>
+    </button>
   );
 }
--- nextjs-course-code-03-prj-routing-starting-setup/pages/events/index.js
+++ nextjs-course-code-03-prj-routing-starting-setup/pages/events/index.js
@@ -1,7 +1,14 @@
+import EventList from "../../components/events/event-list";
+import { getAllEvents } from "../../dummy-data";
+import EventsSearch from "../../components/events/events-search";
+
 export default function AllEventsPage() {
+  const events = getAllEvents();
+
   return (
-    <div>
-      <h1>All Events</h1>
-    </div>
+    <>
+      <EventsSearch />
+      <EventList items={events} />
+    </>
   );
 }

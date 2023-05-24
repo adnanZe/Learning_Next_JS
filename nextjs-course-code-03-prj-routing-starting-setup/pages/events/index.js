0110282a1201ffcc9d1d0e597b8e6c52d5fa26f4
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-05-24 07:46:26 +0300

    construct filtered route

--- nextjs-course-code-03-prj-routing-starting-setup/pages/events/index.js
+++ nextjs-course-code-03-prj-routing-starting-setup/pages/events/index.js
@@ -1,13 +1,21 @@
-import EventList from "../../components/events/event-list";
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../dummy-data";
+import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
 export default function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </>
   );

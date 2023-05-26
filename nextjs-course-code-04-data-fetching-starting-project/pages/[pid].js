e8ca8673bbf8544482d441ac15750aaf602b3b67
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-05-26 17:16:19 +0300

    loading path dynamically

--- nextjs-course-code-04-data-fetching-starting-project/pages/[pid].js
+++ nextjs-course-code-04-data-fetching-starting-project/pages/[pid].js
@@ -14,12 +14,18 @@ export default function ProductDetailPage({ loadedProduct }) {
   );
 }
 
-export async function getStaticProps({ params }) {
-  const productId = params.pid;
-
+async function getData() {
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
+
+  return data;
+}
+
+export async function getStaticProps({ params }) {
+  const productId = params.pid;
+
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
 
@@ -31,8 +37,13 @@ export async function getStaticProps({ params }) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const ids = data.products.map((product) => product.id);
+  const pathsWithParams = ids.map((id) => ({ params: { pid: id } }));
+
   return {
-    paths: [{ params: { pid: "p1" } }],
-    fallback: "blocking",
+    paths: pathsWithParams,
+    fallback: false,
   };
 }

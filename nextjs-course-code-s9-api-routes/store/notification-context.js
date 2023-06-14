3a9c0ecf753d62bc97ef622dc4d36c62f04dce85
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-06-14 21:14:19 +0300

    adding context state

--- nextjs-course-code-s9-api-routes/store/notification-context.js
+++ nextjs-course-code-s9-api-routes/store/notification-context.js
@@ -2,15 +2,29 @@ import { createContext, useState } from "react";
 
 const NotificationContext = createContext({
   notification: null,
-  showNotification: function () {},
+  showNotification: function (notificationData) {},
   hideNotification: function () {},
 });
 
 export function NotificationContextProvider(props) {
-  const [] = useState();
+  const [activeNotification, setActiveNotification] = useState();
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
 
   return (
-    <NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
       {props.children}
     </NotificationContext.Provider>
   );

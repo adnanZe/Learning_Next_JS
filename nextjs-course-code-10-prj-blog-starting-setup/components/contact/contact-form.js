8fb02ba6ecd2afdac0b7c0ad36148755cc7df20a
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2023-07-11 21:26:11 +0300

    [feature] added Contact Api Route

--- nextjs-course-code-10-prj-blog-starting-setup/components/contact/contact-form.js
+++ nextjs-course-code-10-prj-blog-starting-setup/components/contact/contact-form.js
@@ -1,10 +1,16 @@
 import classes from "./contact-form.module.css";
 
 export default function ContactForm() {
+  function sendMessageHandler(event) {
+    event.preventDefault();
+
+    fetch("/api/contact");
+  }
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="">Your Email</label>
